b0c667674d6f4150ac15990f920c1139c07a3db9
heewon <heewon@heewonui-MacBookAir.local>
2026-01-07 19:03:49 +0900

    Init

---
+++ src/main/java/triple_stack/route_in_backend/RouteInBackendApplication.java
@@ -0,0 +1,13 @@
+package triple_stack.route_in_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RouteInBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RouteInBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/triple_stack/route_in_backend/RouteInBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package triple_stack.route_in_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RouteInBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
