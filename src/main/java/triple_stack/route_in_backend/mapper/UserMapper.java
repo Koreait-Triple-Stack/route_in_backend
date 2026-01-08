435e6850022794457642fb2491b58c7238022266
yoonho0624 <qkrdbsgh0624@gmail.com>
2026-01-08 16:49:05 +0900

    Feat : user

--- src/main/java/triple_stack/route_in_backend/mapper/UserMapper.java
+++ src/main/java/triple_stack/route_in_backend/mapper/UserMapper.java
@@ -3,10 +3,15 @@ package triple_stack.route_in_backend.mapper;
 import org.apache.ibatis.annotations.Mapper;
 import triple_stack.route_in_backend.entity.User;
 
+import java.util.List;
 import java.util.Optional;
 
 @Mapper
 public interface UserMapper {
     Optional<User> getUserByUsername(String username);
     Optional<User> getUserByUserId(Integer userId);
+    int addUser(User user);
+    int changeProfileImg(User user);
+    int withdraw(Integer userId);
+    void deleteUser();
 }
