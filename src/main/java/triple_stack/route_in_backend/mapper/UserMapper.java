d0a2f4ef6c10f28651ef871fd9e67801fdfb8e08
yoonho0624 <qkrdbsgh0624@gmail.com>
2026-01-08 16:03:38 +0900

    Feat : user

--- src/main/java/triple_stack/route_in_backend/mapper/UserMapper.java
+++ src/main/java/triple_stack/route_in_backend/mapper/UserMapper.java
@@ -1,4 +1,12 @@
 package triple_stack.route_in_backend.mapper;
 
+import org.apache.ibatis.annotations.Mapper;
+import triple_stack.route_in_backend.entity.User;
+
+import java.util.Optional;
+
+@Mapper
 public interface UserMapper {
+    Optional<User> getUserByUsername(String username);
+    Optional<User> getUserByUserId(Integer userId);
 }
--- src/main/java/triple_stack/route_in_backend/repository/UserRepository.java
+++ src/main/java/triple_stack/route_in_backend/repository/UserRepository.java
@@ -1,4 +1,22 @@
 package triple_stack.route_in_backend.repository;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import triple_stack.route_in_backend.entity.User;
+import triple_stack.route_in_backend.mapper.UserMapper;
+
+import java.util.Optional;
+
+@Repository
 public class UserRepository {
+    @Autowired
+    private UserMapper userMapper;
+
+    public Optional<User> getUserByUsername(String username) {
+        return userMapper.getUserByUsername(username);
+    }
+
+    public Optional<User> getUserByUserId(Integer userId) {
+        return userMapper.getUserByUserId(userId);
+    }
 }
