20b258c8377489cbac908375f0375c3c5cafd007
yoonho0624 <qkrdbsgh0624@gmail.com>
2026-01-09 11:26:02 +0900

    Feat : user

--- src/main/java/triple_stack/route_in_backend/mapper/UserMapper.java
+++ src/main/java/triple_stack/route_in_backend/mapper/UserMapper.java
@@ -3,7 +3,6 @@ package triple_stack.route_in_backend.mapper;
 import org.apache.ibatis.annotations.Mapper;
 import triple_stack.route_in_backend.entity.User;
 
-import java.util.List;
 import java.util.Optional;
 
 @Mapper
@@ -12,7 +11,12 @@ public interface UserMapper {
     Optional<User> getUserByUserId(Integer userId);
     Optional<User> getUserByProviderAndProviderUserId(String provider, String providerUserId);
     int addUser(User user);
+    int changeUsername(User user);
     int changeProfileImg(User user);
+    int changeAddress(User user);
+    int changeHeightAndWeight(User user);
+    int changeCurrentRun(User user);
+    int changeWeeklyRun(User user);
     int withdraw(Integer userId);
     void deleteUser();
 }
--- src/main/java/triple_stack/route_in_backend/repository/UserRepository.java
+++ src/main/java/triple_stack/route_in_backend/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package triple_stack.route_in_backend.repository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Repository;
 import triple_stack.route_in_backend.entity.User;
 import triple_stack.route_in_backend.mapper.UserMapper;
@@ -22,5 +23,49 @@ public class UserRepository {
 
     public Optional<User> getUserByProviderAndProviderUserId(String provider, String providerUserId) {
         return userMapper.getUserByProviderAndProviderUserId(provider, providerUserId);
+    }
+
+    public Optional<User> addUser(User user) {
+        try {
+            int result = userMapper.addUser(user);
+            if (result != 1) {
+                throw new RuntimeException("회원정보 추가에 실패했습니다.");
+            }
+        } catch (DuplicateKeyException e) {
+            return Optional.empty();
+        }
+        return Optional.of(user);
+    }
+
+    public int changeUsername(User user) {
+        return userMapper.changeUsername(user);
+    }
+
+    public int changeProfileImg(User user) {
+        return userMapper.changeProfileImg(user);
+    }
+
+    public int changeAddress(User user) {
+        return userMapper.changeAddress(user);
+    }
+
+    public int changeHeightAndWeight(User user) {
+        return userMapper.changeHeightAndWeight(user);
+    }
+
+    public int changeCurrentRun(User user) {
+        return userMapper.changeCurrentRun(user);
+    }
+
+    public int changeWeeklyRun(User user) {
+        return userMapper.changeWeeklyRun(user);
+    }
+
+    public int withdraw(Integer userId) {
+        return userMapper.withdraw(userId);
+    }
+
+    public void deleteUser() {
+        userMapper.deleteUser();
     }
 }
