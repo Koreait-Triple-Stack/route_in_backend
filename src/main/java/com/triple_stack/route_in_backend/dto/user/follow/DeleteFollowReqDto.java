e46d0fe9cb9c0ff54404f944fd3a34028eee88ba
heewon <heewon@heewonui-MacBookAir.local>
2026-01-10 20:36:54 +0900

    Feat: follow

--- src/main/java/com/triple_stack/route_in_backend/dto/user/follow/DeleteFollowReqDto.java
+++ src/main/java/com/triple_stack/route_in_backend/dto/user/follow/DeleteFollowReqDto.java
@@ -1,5 +1,6 @@
 package com.triple_stack.route_in_backend.dto.user.follow;
 
+import com.triple_stack.route_in_backend.entity.Follow;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,4 +11,11 @@ import lombok.Data;
 public class DeleteFollowReqDto {
     private Integer followerUserId;
     private Integer followingUserId;
+
+    public Follow toEntity() {
+        return Follow.builder()
+                .followerUserId(followerUserId)
+                .followingUserId(followingUserId)
+                .build();
+    }
 }
